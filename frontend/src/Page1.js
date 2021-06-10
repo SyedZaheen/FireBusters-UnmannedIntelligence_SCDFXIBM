955e802ae9d597d1090393c45e1094b10447a0d6
SZaheen60 <82359597+SZaheen60@users.noreply.github.com>
2021-06-10 08:09:04 +0800

    Added a minigame

--- frontend/src/Page1.js
+++ frontend/src/Page1.js
@@ -2,13 +2,28 @@ import React from "react";
 import { Link } from "react-router-dom";
 import FadeIn from "react-fade-in";
 import Col from "react-bootstrap/Col";
+import Button from "react-bootstrap/Button";
 
 export default function Page1() {
   return (
-    <FadeIn delay={200} transitionDuration={800}>
+    <FadeIn transitionDuration="800">
       <div className="whole-screen-center">
-        <div>
-            
+        <div className="d-flex align-items-center flex-column p-5 border border-white">
+          <h1 className="text-center title-text">
+            Welcome to the UFM Controller Dashboard
+          </h1>
+          <p className="subtitle-text text-center mt-5">
+            You will be lead to a page with a dashboard for controlling the UFM.
+            You may use the <b style={{ fontWeight: 900 }}>arrow keys</b> of
+            your keyboard in order to point the head of the UFM.
+ 
+          </p>
+            <br />
+            <br />
+<h2 className="title-text text-center">GOOD LUCK!</h2>
+          <Link to="/dashboard" className="d-block mt-5 btn btn-info">
+            Click here to continue!
+          </Link>
         </div>
       </div>
     </FadeIn>
--- frontend/src/Page2.js
+++ frontend/src/Page2.js
@@ -1,11 +1,95 @@
 import React from "react";
-import Row from 'react-bootstrap/Row'
-import Jumbotron from "react-bootstrap/Jumbotron";
+import { useEffect, useState } from "react";
+import Graph from "./Graph.png";
 
 export default function Page2() {
+  let [topPosition, setTop] = useState(400);
+  let [degrees, setDegrees] = useState({top: 0, left: 0})
+  let [rightPostion, setRight] = useState(480);
+  useEffect(()=> {
+    setDegrees({
+      right: Math.floor((480-rightPostion)/220 * 30 )
+    })
+  }, [rightPostion, topPosition])
+  useEffect(() => {
+    function keyfunction({ key }) {
+      console.log(key, topPosition, rightPostion);
+      switch (key) {
+        case "w":
+          setTop((t) => (t > 100 ? t - 5 : t));
+          break;
+        case "d":
+          setRight((r) => (r > 260 ? r - 5 : r));
+          break;
+        case "s":
+          setTop((t) => (t < 440 ? t + 5 : t));
+          break;
+        case "a":
+          setRight((r) => (r < 700 ? r + 5 : r));
+          break;
+        default:
+          break;
+      }
+    }
+    document.addEventListener("keypress", keyfunction);
+    return () => document.removeEventListener("keypress", keyfunction);
+  }, []);
+
   return (
-    <>
-      
-    </>
+    <div className="d-flex justify-content-between">
+      <div className="p-4">
+        <div className="details font-weight-bold">
+          <p>-- Incident Details --</p>
+          <span>Incident Location:</span>
+          <span className="font-weight-normal">
+            {" "}
+            Tuas View Mall 1.3521° N, 103.8198° E
+          </span>
+          <br />
+          <span>Category:</span>
+          <span className="font-weight-normal"> Level 4 (Fire At Large)</span>
+          <br />
+          <span>Type:</span>
+          <span className="font-weight-normal"> Industrial Fire</span>
+          <br />
+          <span>Ground Command in-charge:</span>
+          <span className="font-weight-normal"> STN 23 ROTA 2 RC</span>
+          <br />
+        </div>
+        <div className="details font-weight-bold mt-5">
+          <p>-- UFM Status --</p>
+          <span>Water Supply:</span>
+          <span className="font-weight-normal text-success font-weight-bolder">
+            {" "}
+            Connected, Activated
+          </span>
+          <br />
+          <span>Power Supply:</span>
+          <span className="font-weight-normal text-success font-weight-bolder">
+            {" "}
+            500/650 MHh
+          </span>
+          <br />
+          <span>Type:</span>
+          <span className="font-weight-normal"> Industrial Fire</span>
+          <br />
+          <span>Ground Command in-charge:</span>
+          <span className="font-weight-normal"> STN 23 ROTA 2 RC</span>
+          <br />
+        </div>{" "}
+      </div>
+      <div className=" position-relative">
+        <img src={Graph} alt="A graph"></img>
+        <div
+          className="position-absolute d-flex justify-content-center align-items-center rounded-circle border-danger border"
+          style={{
+            height: "200px",
+            width: "200px",
+            top: topPosition,
+            right: rightPostion,
+          }}
+        >{degrees.right} degrees</div>
+      </div>
+    </div>
   );
 }
